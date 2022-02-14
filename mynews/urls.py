57006a0567996885690c06b43b99223e808b71e8
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-02-14 10:20:25 +0300

    views

--- mynews/urls.py
+++ mynews/urls.py
@@ -3,5 +3,7 @@ from .import views
 
 #url patterns
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('today', views.news_of_day, name='news_of_day'),
+    path('news/archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news,name = 'pastNews')
 ]
