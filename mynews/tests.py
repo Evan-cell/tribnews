4e6292c6002223b741d15c92149deaf9a9644134
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-02-16 11:55:50 +0300

    testcase nad models

--- mynews/tests.py
+++ mynews/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from .models import Editor,tags,Article
 # Create your tests here.
+
+class EditorTestClass(TestCase):
+    #set up method
+    def setup(self):
+        self.james = Editor(first_name='james', last_naem='kimani',email='malcomechege@gmail.com')
+        
+     # test instance
+    def test_instance(self):
+         self.assertTrue(isinstance(self.james,Editor))    
