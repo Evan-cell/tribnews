9770bb095ca106d15d6cfa3e7cd15bfdfc0e3c71
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-02-21 13:00:14 +0300

    created a search functionality and imported cloudinary

--- mynews/urls.py
+++ mynews/urls.py
@@ -4,5 +4,6 @@ from .import views
 #url patterns
 urlpatterns = [
     path('', views.news_today, name="news_today"),
-    path('news/archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news,name = 'pastNews')
+    path('news/archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news,name = 'pastNews'),
+    path('search/', views.search_results, name='search_results')
 ]
--- mynews/views.py
+++ mynews/views.py
@@ -8,6 +8,7 @@ def news_today(request):
     date = dt.date.today()
     news = Article.todays_news()
     return render(request, 'today-news.html',{'date': date,"news":news})
+print(news_today)
 
 
 
@@ -26,3 +27,13 @@ def past_days_news(request,past_date):
     
     news = Article.days_news(date)
     return render(request, 'past-news.html', {"date":date,"news":news})
+def search_results(request):
+    if 'article' in request.GET and request.GET["article"]:
+        search_term = request.GET.get("article")
+        searched_articles = Article.search_by_title(search_term)
+        message = f"{search_term}"
+        
+        return render(request, 'search.html',{"message":message,"articles": searched_articles})
+    else:
+        message = "you haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
