177f43fdbf344e00c81ae7f3fd4ffb9c5a3fceae
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-02-21 14:29:13 +0300

    cloudinary imports

--- news/settings.py
+++ news/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
